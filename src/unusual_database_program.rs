5c538f01366c232f072f7f81fe0f6e6030dccad9
lodrem <jialun.cai@pm.me>
2023-01-09 22:02:14 +0800

    Add version

--- src/unusual_database_program.rs
+++ src/unusual_database_program.rs
@@ -24,6 +24,12 @@ pub async fn run(addr: SocketAddr) -> Result<()> {
                     info!("Inserting key-value pair: {} = {}", k, v);
                     kv.write().unwrap().insert(k.to_owned(), v.to_owned());
                 }
+                None if &req == "version" => {
+                    const VERSION: &'static str = "version=Ken's Key-Value Store 1.0";
+                    if let Err(e) = socket.send_to(VERSION.as_bytes(), remote_addr).await {
+                        warn!("Failed to send response to {}: {:?}", remote_addr, e);
+                    }
+                }
                 None => {
                     info!("Querying key-value pair: {}", req);
                     let resp = {
