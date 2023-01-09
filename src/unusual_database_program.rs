42088c825407b8b40a4379402e0d8d1599460e6f
lodrem <jialun.cai@pm.me>
2023-01-09 22:07:44 +0800

    Debug

--- src/unusual_database_program.rs
+++ src/unusual_database_program.rs
@@ -19,13 +19,14 @@ pub async fn run(addr: SocketAddr) -> Result<()> {
         let kv = kv.clone();
         tokio::spawn(async move {
             let req = String::from_utf8_lossy(&buf[0..n]).to_string();
+            info!("Received {} from {}", req, remote_addr);
             match req.split_once('=') {
                 Some((k, v)) => {
                     info!("Inserting key-value pair: {} = {}", k, v);
                     kv.write().unwrap().insert(k.to_owned(), v.to_owned());
                 }
                 None if &req == "version" => {
-                    const VERSION: &'static str = "version=Ken's Key-Value Store 1.0";
+                    const VERSION: &'static str = "version=Lodrem's Key-Value Store 1.0";
                     if let Err(e) = socket.send_to(VERSION.as_bytes(), remote_addr).await {
                         warn!("Failed to send response to {}: {:?}", remote_addr, e);
                     }
